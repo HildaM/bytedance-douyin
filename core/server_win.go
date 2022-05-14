9b52e97fba4a669cfd639c67016e41c32ea9dce0
Quan <hilda_quan@163.com>
2022-05-14 18:56:42 +0800

    update: import pprof to monitor

--- core/server_win.go
+++ core/server_win.go
@@ -1,8 +1,10 @@
 package core
 
 import (
+	"bytedance-douyin/global"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"runtime"
 	"time"
 )
 
@@ -21,4 +23,18 @@ func initServer(address string, router *gin.Engine) server {
 		WriteTimeout:   20 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
+}
+
+func initPprof() {
+	runtime.SetMutexProfileFraction(1) // 开启对锁调用的跟踪
+	runtime.SetBlockProfileRate(1)     // 开启对阻塞操作的跟踪
+
+	// 引入pprof
+	go func() {
+		// 启动一个 http server，注意 pprof 相关的 handler 已经自动注册过了
+		global.GVA_LOG.Info("Starting pprof to monitor this server")
+		if err := http.ListenAndServe(":6060", nil); err != nil {
+			global.GVA_LOG.Error(err.Error())
+		}
+	}()
 }
